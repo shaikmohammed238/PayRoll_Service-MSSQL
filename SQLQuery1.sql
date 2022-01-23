946b474f8dfda47b9abaa95134483c6d4762aea4
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2022-01-23 20:08:10 +0530

    added UC1

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,6 +1,11 @@
+----- Use Case 1 Ability to create a payroll service database.
+----- Using MSSQL Client use create database query to create a payroll_service database.
+----- Also you can see the DB created by using show database query.
+----- And go to the database created by using use payroll_service query. 
 
+create database Payroll_Service;
+use Payroll_Service;
+exec sp_databases;  
 
-
-
-create database Payroll_Service
-use Payroll_Service
+select * 
+from sys.databases;
